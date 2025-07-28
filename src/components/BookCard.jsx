2e5ed649e73b4ae34a446f2b3dab666dd5d10b57
s1m0nDevs <s1m0nkyivstar@gmail.com>
2025-07-29 02:23:36 +0300

    small styles update

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
-import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import React from "react"
+import { Card, Icon, Image, Button } from "semantic-ui-react"
 
 const BookCard = (props) => {
-
-  const {id, title, author, price, image, addToCart, incrementAmount, amount} = props
+  const { id, title, author, price, image, addToCart, incrementAmount, amount } = props
 
   const [isImageLoaded, setIsImageLoaded] = React.useState(false)
 
-  const handleClick = ()=>{
-    addToCart(props);
-    incrementAmount({id, amount:amount?amount:1});
+  const handleClick = () => {
+    addToCart(props)
+    incrementAmount({ id, amount: amount ? amount : 1 })
   }
 
   return (
-    <Card>
-      <div className="preloader"
-        style={{display: !isImageLoaded ? "flex" : "none"}}>
-      </div>
-      <Image 
+    <Card className="book-card">
+      <div
+        className="preloader"
+        style={{ display: !isImageLoaded ? "flex" : "none" }}
+      ></div>
+      <Image
         src={image}
-        style={isImageLoaded?{}:{display:'none'}}
-        onLoad={()=>{setIsImageLoaded(true)}}
-        onError={(e)=>{e.target.src="https://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png"}}
+        style={isImageLoaded ? {} : { display: "none" }}
+        onLoad={() => {
+          setIsImageLoaded(true)
+        }}
+        onError={(e) => {
+          e.target.src =
+            "https://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png"
+        }}
       />
       <Card.Content>
         <Card.Header>{title}</Card.Header>
         <Card.Meta>
-          <span className='date'>{author}</span>
+          <span className="date">{author}</span>
         </Card.Meta>
       </Card.Content>
       <Card.Content extra>
         <a>
-          <Icon name='rub' />
+          <Icon name="rub" />
           {price}
         </a>
       </Card.Content>
-      <Button onClick={handleClick}>Добавить в корзину{amount>0 && `(${amount})`}</Button>
+      <Button onClick={handleClick}>
+        Добавить в корзину{amount > 0 && `(${amount})`}
+      </Button>
     </Card>
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,34 +1,53 @@
-import React from 'react'
-import {Menu, List, Popup, Button, Image, Label} from 'semantic-ui-react'
+import React from "react"
+import { Menu, List, Popup, Button, Image, Label } from "semantic-ui-react"
 
-const CartComponent = ({amount, title, id, image, removeFromCart, incrementAmount, decrementAmount})=>{
-
-  console.log(amount)
-
+const CartComponent = ({
+  amount,
+  title,
+  id,
+  image,
+  removeFromCart,
+  incrementAmount,
+  decrementAmount,
+}) => {
   return (
-  <List selection divided verticalAlign="middle">
-    <List.Item className="list-item">
-      <List.Content>{title}</List.Content>
+    <List selection divided verticalAlign="middle">
+      <List.Item className="list-item">
+        <List.Content>{title}</List.Content>
         {/* <Image avatar src={image} /> */}
-      <List.Content className="amount-controller">
-        <Button basic onClick={()=>{amount>1?decrementAmount(id):removeFromCart(id)}}>-</Button>
-        <Label>{amount}</Label>
-        <Button basic onClick={()=>{incrementAmount({id,amount})}}>+</Button>
-      </List.Content>
-      <List.Content>
-        <Button onClick={removeFromCart.bind(null, id)} color="red">Удалить</Button>
-      </List.Content>
-    </List.Item>
-  </List>
-)};
+        <List.Content className="amount-controller">
+          <Button
+            basic
+            onClick={() => {
+              amount > 1 ? decrementAmount(id) : removeFromCart(id)
+            }}
+          >
+            -
+          </Button>
+          <Label>{amount}</Label>
+          <Button
+            basic
+            onClick={() => {
+              incrementAmount({ id, amount })
+            }}
+          >
+            +
+          </Button>
+        </List.Content>
+        <List.Content>
+          <Button onClick={removeFromCart.bind(null, id)} color="red">
+            Удалить
+          </Button>
+        </List.Content>
+      </List.Item>
+    </List>
+  )
+}
 
-const MenuComponent = ({totalPrice, totalBooks, items, amountItems}) => {
-
+const MenuComponent = ({ totalPrice, totalBooks, items, amountItems }) => {
   return (
     <Menu>
-      <Menu.Item>
-        Магазин книг
-      </Menu.Item>
+      <Menu.Item>Магазин книг</Menu.Item>
 
       <Menu.Menu position="right">
         <Menu.Item>
@@ -40,16 +59,21 @@ const MenuComponent = ({totalPrice, totalBooks, items, amountItems}) => {
           trigger={
             <Menu.Item>
               Корзина(<b>{totalBooks}</b>)
-            </Menu.Item> 
+            </Menu.Item>
           }
-          content = {
-            items.length? 
-              items.map(book=><CartComponent 
-                key={book.id} 
-                {...book}
-                amount={amountItems.length&&amountItems.find(item=>item.id===book.id).amount}
-              />)
-            : 'Корзина пустая.'
+          content={
+            items.length
+              ? items.map((book) => (
+                  <CartComponent
+                    key={book.id}
+                    {...book}
+                    amount={
+                      amountItems.length &&
+                      amountItems.find((item) => item.id === book.id).amount
+                    }
+                  />
+                ))
+              : "Корзина пустая."
           }
           on="click"
           hideOnScroll
